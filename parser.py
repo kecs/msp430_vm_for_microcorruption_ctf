b7bea29bec82d667a31eda4299474b112936ad0c
Mihály Gábor <mihaly.gabor.dev@gmail.com>
2018-12-03 14:49:59 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,43 +1,62 @@
 import re
 import pdb
+from collections import OrderedDict
 
-from instructions import *
+import instructions
 
 
-# TODO: there are basically 2 type of regexes............
-PATTERNS = {
-    'mov\s+#*(@*\w+),\s+(\w+)': mov,
-    'add\s+#*(@*\w+),\s+(\w+)': add,
-    'sub\s+#*(@*\w+),\s+(\w+)': sub,
-    '_and\s+#*(@*\w+),\s+(\w+)': _and,
-    'inc\s+(\w+)': inc,
-    'dec\s+(\w+)': dec,
-    'clr\s+(\w+)': clr,
-    'sxt\s+(\w+)': sxt,
-    'push\s+(\w+)': push,
-    'pop\s+(\w+)': pop,
-    'ret\s+(\w+)': ret,
+def load(instruction_name):
+    " Translate asm fn name to python fn name, handle reserved. "
+    
+    if instruction_name.endswith('.b'):
+        return getattr(instructions, instruction_name.replace('.', ''))
+    if instruction_name == 'and':
+        return instructions.and_
+    else:
+        return getattr(instructions, instruction_name)
 
-}
 
-PT = {1: '\s+(\w+)', 2: '\s+#*(@*\w+),\s+(\w+)'}
+OP2_OPERANDS = OrderedDict((
+    # Replaces 0x0(r) with @r
+    ('\s+#?0x0\((\w+)\),\s+0x0\((\w+)\)', lambda f, a, b: (load(f), '@' + a, '@' + b)),
+    ('\s+#?0x0\((\w+)\),\s+(\w+)', lambda f, a, b: (load(f), '@' + a, b)),
+    ('\s+#?(\w+),\s+0x0\((\w+)\)', lambda f, a, b: (load(f), a, '@' + b)),
+    
+    # Replace 0x33(r) with @r+33
+    ('\s+#?(\w+)\((\w+)\),\s+(\w+)\((\w+)\)', lambda f, offs1, a, offs2, b: (load(f), '@{}+{}'.format(a, offs1), '@{}+{}'.format(b, offs2))),
+    ('\s+#?(\w+),\s+(\w+)\((\w+)\)', lambda f, a, offs, b: (load(f), a, '@{}+{}'.format(b, offs))),
+    ('\s+#?(\w+)\((\w+)\),\s+(\w+)', lambda f, offs, a, b: (load(f), '@{}+{}'.format(a, offs), b)),
+    
+    # TODO: it's swallowing + for @rxx+
+    ('\s+#?(@*\w+)\+?,\s+(\w+)', lambda f, a, b: (load(f), a, b)),
+))
+
+OP2_PATTERNS = OrderedDict(
+    (('(mov|mov\.b|add|sub|and|cmp|cmp\.b)' + k, v) for k, v in OP2_OPERANDS.items())
+)
+
+OP1_PATTERNS = OrderedDict({
+    '(inc|dec|push|pop|clr|sxt|call|ret|tst|tst\.b|jmp|jz|jnz|jl|jge|jne)\s+(\w+)': lambda f, arg: (load(f), arg),
+})
 
 def cast(s):
     m = re.search('0x(\w+)', s)
-    if m:
+    if m and ('+' not in s):
         return int(m.group(), 16)
     else:
         return s
 
+OP_PATTERNS = OrderedDict()
+OP_PATTERNS.update(OP1_PATTERNS)
+OP_PATTERNS.update(OP2_PATTERNS)
 
 def parse_asm_line(line):
-    for regex, fn in PATTERNS.items():
+    for regex, fn in OP_PATTERNS.items():
         m = re.search(regex, line)
-        if m and len(m.groups()) == 1:
-            return (fn, cast(m.groups[0]))
-        elif m and len(m.groups()) == 2:
-            return (fn, cast(m.groups()[0]), cast(m.groups()[1]))
-
-        
+        if m:
+            # print('[*] ', m.groups())
+            return fn(*[cast(s) for s in m.groups()])
+            
 def parse_asm_lines(s):
     return [parse_asm_line(l) for l in s.split('\n') if l.strip()]
+
